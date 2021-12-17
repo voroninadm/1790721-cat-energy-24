50f5067f632f40e07b75cf06c9beb8185b01d70f
Alex <voronin.adm@mail.ru>
2021-12-17 23:47:51 +0300

    делает список ссылок в подвале

--- source/templates/data.js
+++ source/templates/data.js
@@ -3,5 +3,14 @@ export default {
     { "id": 0, "path": "index.html", "name": "Главная" },
     { "id": 1, "path": "catalog.html", "name": "Каталог продукции" },
     { "id": 2, "path": "form.html", "name": "Подбор программы" }
+  ],
+
+  //testing links
+  "links": [
+    { "id": 0, "source": "https://vk.com/htmlacademy/", "name": "ВКонтакте" },
+    { "id": 1, "source": "https://www.instagram.com/htmlacademy", "name": "Instagram" },
+    { "id": 2, "source": "https://www.facebook.com/htmlacademy", "name": "Facebook" }
   ]
+
 }
+
