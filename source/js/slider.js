d8f7db17bf41d6796e820c7bba7c59e430540a1a
Alex <voronin.adm@mail.ru>
2021-12-30 01:03:25 +0300

    пробует оживить слайдер

--- source/js/slider.js
+++ source/js/slider.js
@@ -2,14 +2,51 @@ let sliderButtonBefore = document.querySelector('.control__button--before');
 let sliderButtonAfter = document.querySelector('.control__button--after');
 let sliderToggle = document.querySelector('.control__range');
 let sliderCanvas = document.querySelector('.slider');
+let scale = document.querySelector('.control__scale');
 
 
-sliderButtonBefore.onclick = function() {
+sliderButtonBefore.onclick = function () {
   sliderToggle.classList.remove('control__range--move');
   sliderCanvas.style.setProperty('--current-value', '1');
 }
 
-sliderButtonAfter.onclick = function() {
+sliderButtonAfter.onclick = function () {
   sliderToggle.classList.add('control__range--move');
   sliderCanvas.style.setProperty('--current-value', '0');
 }
+
+
+//slider-function
+sliderToggle.onmousedown = function(event) {
+
+  let shiftX = event.clientX - (sliderToggle.getBoundingClientRect().left - 83);
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+
+  function onMouseMove(event) {
+
+    let newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left;
+
+    // toggle only in scale
+    if (newLeft < 0) {
+      newLeft = 0;
+    }
+    let rightEdge = scale.offsetWidth - sliderToggle.offsetWidth;
+    if (newLeft > rightEdge) {
+      newLeft = rightEdge;
+    }
+
+    sliderToggle.style.left = newLeft + 'px';
+    let toggleCurrent = sliderToggle.style.left = ((newLeft) / 392);
+    sliderCanvas.style.setProperty('--current-value', toggleCurrent);
+
+  }
+
+  function onMouseUp() {
+    document.removeEventListener('mouseup', onMouseUp);
+    document.removeEventListener('mousemove', onMouseMove);
+  }
+
+};
+
