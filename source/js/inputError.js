0410b066b742c23990bbc5b3bb7dee0ad92607e8
Alex <voronin.adm@mail.ru>
2021-12-30 13:11:40 +0300

    добавляет скрипт для обязательных полей

---
+++ source/js/inputError.js
@@ -0,0 +1,13 @@
+//Проверка формы
+let inputReq = document.querySelectorAll(".form-group__input[required]");
+let btnSubmit = document.querySelector(".form__button");
+
+btnSubmit.onclick = function() {
+  for (let i=0; i<inputReq.length; i++){
+    if (inputReq[i].value == "") {
+      inputReq[i].classList.add("form-group__input--error");
+    } else {
+      inputReq[i].classList.remove("form-group__input--error");
+    }
+  }
+};
