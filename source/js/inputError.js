ae6ededada2a976aa7516ee42337954313b201ff
Alex <voronin.adm@mail.ru>
2022-01-04 23:58:22 +0300

    мелкие правки

--- source/js/inputError.js
+++ source/js/inputError.js
@@ -2,13 +2,13 @@
 let inputReq = document.querySelectorAll(".form-group__input[required]");
 let btnSubmit = document.querySelector(".form__button");
 
-btnSubmit.addEventListerner("click", function () {
+btnSubmit.onclick = function() {
   for (let i=0; i<inputReq.length; i++){
-    if (inputReq[i].value === "") {
+    if (inputReq[i].value == "") {
       inputReq[i].classList.add("form-group__input--error");
     } else {
       inputReq[i].classList.remove("form-group__input--error");
     }
   }
-})
-;
+};
+я
