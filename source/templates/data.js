33243fa73d25a7b6bce3f3ecfc64d223e9e289a3
Alex <voronin.adm@mail.ru>
2022-01-06 18:45:27 +0300

    каталог циклом часть 1

--- source/templates/data.js
+++ source/templates/data.js
@@ -10,5 +10,16 @@ export default {
     {"source": "https://vk.com/htmlacademy/", "name": "ВКонтакте", "id": "vk" },
     {"source": "https://www.instagram.com/htmlacademy", "name": "Instagram", "id": "ig" },
     {"source": "https://www.facebook.com/htmlacademy", "name": "Facebook", "id": "fb" }
+  ],
+
+  //for catalog items
+  "items": [
+    {"title": "PRO", "mass": "500", "taste": "Курица", "price": "700", "class": "product-block__image product-block__image--small", "name": "chicken_s", "format": "png", "path": "./img/catalog/", "alt": "Cat Energy Pro 500г с курицей", "width": "68", "height": "78"},
+    {"title": "PRO", "mass": "1000", "taste": "Курица", "price": "1000", "class": "product-block__image product-block__image--big", "name": "chicken_xl", "format": "png", "path": "./img/catalog/", "alt": "Cat Energy Pro 1000г с курицей", "width": "84", "height": "100"},
+    {"title": "PRO", "mass": "500", "taste": "Рыба", "price": "700", "class": "product-block__image product-block__image--small", "name": "fish_s", "format": "png", "path": "./img/catalog/", "alt": "Cat Energy Pro 500г с рыбой", "width": "68", "height": "78"},
+    {"title": "PRO", "mass": "1000", "taste": "Рыба", "price": "1000", "class": "product-block__image product-block__image--big", "name": "fish_xl", "format": "png", "path": "./img/catalog/", "alt": "Cat Energy Pro 1000г с рыбой", "width": "84", "height": "100"},
+    {"title": "Slim", "mass": "500", "taste": "Гречка", "price": "400", "class": "product-block__image product-block__image--small", "name": "buckwheat_s", "format": "png", "path": "./img/catalog/", "alt": "Cat Energy Pro 500г с гречкой", "width": "68", "height": "78"},
+    {"title": "Slim", "mass": "1000", "taste": "Гречка", "price": "700", "class": "product-block__image product-block__image--big", "name": "buckwheat_xl", "format": "png", "path": "./img/catalog/", "alt": "Cat Energy Pro 1000г с гречкой", "width": "84", "height": "100"},
+    {"title": "Slim", "mass": "500", "taste": "Рис", "price": "500", "class": "product-block__image product-block__image--small", "name": "rice_s", "format": "png", "path": "./img/catalog/", "alt": "Cat Energy Pro 500г с рисом", "width": "68", "height": "78"}
   ]
 }
